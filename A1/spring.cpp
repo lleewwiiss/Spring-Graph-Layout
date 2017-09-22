700a41d2171d93d9a43d62103cd3e0a40087162b
Lewis Smith <lleewwiisss@gmail.com>
2017-09-22 18:44:16 +1000

    fixed so force interaction is working correctly

--- A1/spring.cpp
+++ A1/spring.cpp
@@ -5,37 +5,38 @@
 #include <sstream>
 #include <vector>
 #include <cmath>
+#include <ctime>
 
 
-double beta = 0.000001;
+double beta = -2.0;
 int frames = 0;
 int nodes = 0;
 
 struct node {
-    double x, y, force;
+    double x, y, force_x, force_y;
     int id;
 };
 
-struct edge {
-    node start, end;
-    double distance, difference;
-};
-
-inline bool operator<(const node &l, const node &r) {
-    return (l.id < r.id);
-}
-
-inline bool operator==(const node &l, const node &r) {
+bool operator==(const node &l, const node &r) {
     return (l.id == r.id);
 }
 
-inline bool operator<(const edge &l, const edge &r) {
-    return (l.distance < r.distance);
+bool operator<(const node &l, const node &r) {
+    return (l.id < r.id);
 }
 
-inline bool operator==(const edge &l, const edge &r) {
-    return (l.start.id == r.start.id && l.end.id == r.end.id);
+struct edge {
+    node start, end;
+    double distance, dx, dy;
+};
+
+
+bool operator<(const edge &l, const edge &r) {
+    return (l.end.id != r.end.id);
 }
+
+
+
 
 class graph {
 private:
@@ -50,8 +51,9 @@ public:
         edge e{};
         e.start = v;
         e.end = u;
-        e.distance = sqrt(pow((u.x - v.x), 2) + pow((u.y - v.y), 2));
-        e.difference = ((u.x - v.x) + (u.y - v.y))/e.distance;
+        e.dx = (u.x - v.x);
+        e.dy = (u.y - v.y);
+        e.distance = e.dx * e.dx + e.dy * e.dy;
         graph_container[v].insert(e);
     }
 
@@ -69,39 +71,45 @@ public:
 void calc_force(graph &g) {
     std::map<node, std::set<edge>> map = g.get_graph();
     std::map<node, std::set<edge>> new_map;
+    //loop through each node in graph
     for (auto it : map) {
-        double coulomb = 0.0;
-        double hook = 0.0;
+        double coulomb_x = 0.0, coulomb_y = 0.0;
+        double hook_x = 0.0, hook_y = 0.0;
+        //get hook forces for each edge
         for (auto &it2 : it.second) {
-            coulomb += (beta / pow(it2.distance, 2)) * it2.difference;
-            hook += -1.0 * (it2.distance - 0.05) * it2.difference;
+            hook_x += 1.0 * (sqrt(it2.distance) - 2.0) * (it2.dx / sqrt(it2.distance));
+            hook_y += 1.0 * (sqrt(it2.distance) - 2.0) * (it2.dy / sqrt(it2.distance));
+        }
+        //get coulomb forces for all other nodes
+        for (auto &it3 : map) {
+            double dx, dy, dist;
+            //don't consider set
+            if (it3.first.id != it.first.id) {
+                dx = it3.first.x - it.first.x;
+                dy = it3.first.y - it.first.y;
+                dist = dx * dx + dy * dy;
+                coulomb_x += (beta / dist) * (dx / sqrt(dist));
+                coulomb_y += (beta / dist) * (dy / sqrt(dist));
+            }
         }
         node t = it.first;
         std::set<edge> e = it.second;
-        t.force = coulomb + hook;
+        t.force_x = coulomb_x + hook_x;
+        t.force_y = coulomb_y + hook_y;
         new_map[t] = e;
     }
     g.set_graph(new_map);
 }
 
-double equilibrium(const graph &g) {
-    std::map<node, std::set<edge>> map = g.get_graph();
-    double force = 0.0;
-    for (auto it : map) {
-        force += fabs(it.first.force);
-    }
-    return force;
-}
 
 void move(graph &g) {
     std::map<node, std::set<edge>> map = g.get_graph();
     std::map<node, std::set<edge>> new_map;
-
     for (const auto &it : map) {
         node n{};
         n.id = it.first.id;
-        n.x = it.first.x + 0.01 * it.first.force;
-        n.y = it.first.y + 0.01 * it.first.force;
+        n.x = it.first.x + 0.05 * it.first.force_x;
+        n.y = it.first.y + 0.05 * it.first.force_y;
         new_map[n] = it.second;
     }
     g.set_graph(new_map);
@@ -131,13 +139,12 @@ void output_graph(const graph &g, int i) {
 
 int balance_graph(graph &g){
     int i = 0;
-    //double force = equilibrium(g);
+
     while (i < frames) {
         move(g);
         update_edges(g);
         calc_force(g);
         output_graph(g, i++);
-        //force = equilibrium(g);
     }
 
 
@@ -148,6 +155,11 @@ int main(int argc, char **argv) {
 
     std::string line, file;
     std::string::size_type sz;
+
+    std::clock_t start;
+    double duration;
+
+    start = std::clock();
 
     int i = 0, j = 0;
 
@@ -201,6 +213,9 @@ int main(int argc, char **argv) {
     }
 
     calc_force(g);
+
+    duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
+    std::cout << "printf: " << duration << '\n';
 
     return balance_graph(g);
 }
